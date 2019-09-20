659ba1367c79f91a102ec31805526a3050aeb251
traburiss <marlycool@qq.com>
2019-09-20 21:20:50 +0800

    stage 2

--- src/main/java/com/traburiss/iq/Solution.java
+++ src/main/java/com/traburiss/iq/Solution.java
@@ -3,7 +3,6 @@ package com.traburiss.iq;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -13,10 +12,14 @@ import java.util.List;
 @Slf4j
 public enum  Solution {
     instance;
-    private static char[][] digits = {
-            {},{},{'a','b','c'}, {'d','e','f'},
-            {'g','h','i'},{'j','k','l'}, {'m','n','o'},
-            {'p','q','r','s'},{'t','u','v'},{'w','x','y','z'}};
+    Solution(){
+        for (int i = 0; i < digitsStage2.length; i++) {
+            int i1 = i%digits.length;
+            digitsStage2[i] = digits[i1];
+        }
+    }
+    private final String[] digits = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    private final String[] digitsStage2 = new String[100];
 
     /**
      * the first solution
@@ -25,10 +28,23 @@ public enum  Solution {
     public List<String> solution(int[] nums){
         List<String> resList = new ArrayList<String>();
         if (nums.length > 0){
-            solution(nums, 0, resList, "");
+            solution(nums, digits,0, resList, "");
         }
         return resList;
     }
+
+    /**
+     * the second solution
+     * @param nums integer array , need in [0,99]
+     */
+    public List<String> solutionStage2(int[] nums){
+        List<String> resList = new ArrayList<String>();
+        if (nums.length > 0){
+            solution(nums, digitsStage2,0,resList, "");
+        }
+        return resList;
+    }
+
     /**
      * real solution
      * @param nums integer array , need in [0,9]
@@ -36,16 +52,21 @@ public enum  Solution {
      * @param resList the result list
      * @param res the result
      */
-    private void solution(int[] nums, int index, List<String> resList, String res){
+    private void solution(int[] nums, String[] digits, int index, List<String> resList, String res){
         if (index < nums.length){
             int num = nums[index];
-            if (num < 0 || num > digits.length) throw new IndexOutOfBoundsException("num should in [0,9]");
-            else if (num == 0 || num == 1){
-                solution(nums, ++index, resList, res);
-            } else {
-                char[] letterList = digits[num];
-                for (char letter : letterList) {
-                    solution(nums, index + 1, resList, res + letter);
+            if (num < 0 || num >= digits.length) {
+                throw new IndexOutOfBoundsException("nums should in [0,"  + (digits.length - 1) + "]");
+            }
+            else {
+                String letterList = digits[num];
+                if (letterList.length() == 0){
+                    solution(nums, digits, ++index, resList, res);
+                }else {
+                    for (int i = 0; i < letterList.length(); i++) {
+                        char letter = letterList.charAt(i);
+                        solution(nums, digits, index + 1, resList, res + letter);
+                    }
                 }
             }
         }else {
--- src/test/java/com/traburiss/iq/SolutionTest.java
+++ src/test/java/com/traburiss/iq/SolutionTest.java
@@ -21,7 +21,9 @@ public class SolutionTest {
     public void solutionTest(){
 
         List<String> result;
-        result = Solution.instance.solution(new int[]{1,2,3});
+        result = Solution.instance.solution(new int[]{2,3});
+        log.info("result1 is {}", result);
+        result = Solution.instance.solution(new int[]{1,0});
         log.info("result1 is {}", result);
         result = Solution.instance.solution(new int[]{0,2,3});
         log.info("result2 is {}", result);
@@ -29,7 +31,25 @@ public class SolutionTest {
         log.info("result3 is {}", result);
 
         expect.expect(IndexOutOfBoundsException.class);
-        expect.expectMessage("num should in [0,9]");
+        expect.expectMessage("nums should in [0,9]");
         Solution.instance.solution(new int[]{0,1,2,11,2,1,0});
+    }
+
+    @Test
+    public void solutionStage2Test(){
+
+        List<String> result;
+        result = Solution.instance.solutionStage2(new int[]{33,2,98});
+        log.info("result1 is {}", result);
+        result = Solution.instance.solutionStage2(new int[]{1,11,21});
+        log.info("result1 is {}", result);
+        result = Solution.instance.solutionStage2(new int[]{0,2,3,99});
+        log.info("result2 is {}", result);
+        result = Solution.instance.solutionStage2(new int[]{0,1,2,3,11,2,1,0});
+        log.info("result3 is {}", result);
+
+        expect.expect(IndexOutOfBoundsException.class);
+        expect.expectMessage("nums should in [0,99]");
+        Solution.instance.solutionStage2(new int[]{0,1,2,100,2,1,0});
     }
 }
